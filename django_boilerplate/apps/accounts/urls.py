677efe3d9ebdf70fe921b90f90b00977ae4fedb1
Jorge Capona <jcapona@gmail.com>
2019-10-14 13:07:48 -0300

    Add user signup view

--- django_boilerplate/apps/accounts/urls.py
+++ django_boilerplate/apps/accounts/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
 from django.urls import include
+from django_boilerplate.apps.accounts.views import signup
+
 
 urlpatterns = [
     path('', include('django.contrib.auth.urls')),
+    path('signup/', signup, name='signup'),
 ]
 
 
