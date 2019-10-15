3ea73856ac3ff8f3ab23d8fc9b6f2f466f20b750
Jorge Capona <jcapona@gmail.com>
2019-10-15 11:28:33 -0300

    Move templates to project folder

--- django_boilerplate/settings.py
+++ django_boilerplate/settings.py
@@ -57,7 +57,7 @@ ROOT_URLCONF = 'django_boilerplate.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'templates')],
+        'DIRS': [os.path.join(BASE_DIR, 'django_boilerplate', 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
